24407f077cf37a0b275af5b37eb6645503624684
Christian Lan <christian.lan.external@atos.net>
2019-07-22 15:53:41 +0200

    first commit

---
+++ src/app/components/tienda/tienda.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'tienda',
+	templateUrl: './tienda.component.html',
+	styleUrls: ['./tienda.component.css']
+})
+
+export class TiendaComponent {
+	public title = 'Curso de Angular 4 avanzado';
+}
